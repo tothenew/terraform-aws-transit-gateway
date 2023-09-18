70f0ad218422c1384c410bf6c9a22bc85ac25ef1
Ayush Kumar Singh <53579697+ayushcloudops@users.noreply.github.com>
2023-09-19 02:05:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,24 +5,5 @@ module "transit_gateway" {
 module "transit_gateway_vpc_attachments" {
   source = "./tgw-attachment"
   transit_gateway_id    =   module.transit_gateway.transit_gateway_id
-  vpc_attachments = [
-    {
-      vpc_id       = "vpc-02b6dfe8702127582"
-      subnet_ids   = ["subnet-044e025aace130945"]
-      dns_support  = "enable"
-      ipv6_support = "disable"
-      additional_tags = {
-        Name = "MyVPC1Attachment"
-      }
-    },
-    {
-      vpc_id       = "vpc-07148506b764133b6"
-      subnet_ids   = ["subnet-0d2f854b3e21392d3"]
-      dns_support  = "enable"
-      ipv6_support = "disable"
-      additional_tags = {
-        Name = "MyVPC2Attachment"
-      }
-    }
-  ]
+  vpc_attachments    = var.vpc_attachments
 }
