91103691130f9d2f1d3c7b3a8cd8b2f03824dd9e
Ayush Kumar Singh <53579697+ayushcloudops@users.noreply.github.com>
2023-09-19 02:05:26 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -3,3 +3,14 @@ variable "name" {
   type        = string
   default     = ""
 }
+
+variable "vpc_attachments" {
+  description = "List of VPC configurations to attach to the Transit Gateway."
+  type = list(object({
+    vpc_id             = string
+    subnet_ids         = list(string)
+    dns_support        = string
+    ipv6_support       = string
+    additional_tags    = map(string)
+  }))
+}
