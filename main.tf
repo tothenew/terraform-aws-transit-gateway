733086bde571dd366450cabe3d545baa093e446b
Ayush Kumar Singh <53579697+ayushcloudops@users.noreply.github.com>
2023-09-19 14:33:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "transit_gateway" {
-    source = "./tgw"
+    source              = "./tgw"
 }
 
 module "transit_gateway_vpc_attachments" {
-  source = "./tgw-attachment"
+  source                = "./tgw-attachment"
   transit_gateway_id    =   module.transit_gateway.transit_gateway_id
-  vpc_attachments    = var.vpc_attachments
+  vpc_attachments       = var.vpc_attachments
 }
