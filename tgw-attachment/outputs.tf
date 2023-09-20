254f917301a7789bd453697fc612ce3924a695f5
Ayush Kumar Singh <manu.ayush.99@gmail.com>
2023-09-20 19:02:45 +0530

    added routes

--- tgw-attachment/outputs.tf
+++ tgw-attachment/outputs.tf
@@ -2,3 +2,4 @@ output "vpc_attachment_ids" {
   description = "Map of VPC attachment IDs."
   value       = { for k, v in aws_ec2_transit_gateway_vpc_attachment.vpc_attachment : k => v.id }
 }
+
--- tgw-routes/var.tf
+++ tgw-routes/var.tf
@@ -1,22 +1,3 @@
-variable "name" {
-  description = "Name to be used on all the resources as identifier"
-  type        = string
-  default     = ""
-}
-
-variable "vpc_attachments" {
-  description = "List of VPC configurations to attach to the Transit Gateway."
-  type = list(object({
-    vpc_id             = string
-    subnet_ids         = list(string)
-    dns_support        = string
-    ipv6_support       = string
-    additional_tags    = map(string)
-  }))
-}
-<<<<<<< HEAD
-=======
-
 variable "create_tgw_route_table" {
   description = "Boolean to determine if a new Transit Gateway Route Table should be created."
   type        = bool
@@ -38,6 +19,11 @@ variable "tgw_routes" {
   default = []
 }
 
+variable "transit_gateway_id" {
+  description = "ID of the Transit Gateway"
+  type        = string
+}
+
 variable "transit_gateway_attachment_id" {
   description = "ID of the Transit Gateway VPC Attachment"
   type        = string
@@ -47,4 +33,3 @@ variable "transit_gateway_route_table_id" {
   description = "ID of the Transit Gateway Route Table"
   type        = string
 }
->>>>>>> 59253ee (added routes)
