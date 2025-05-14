a26e6550336309f34ecfe21010dc344721df5bbc
dhruvguptaTTN <dhruv.gupta@tothenew.com>
2025-05-14 17:41:18 +0530

    Update main.tf

--- example/tgw-routes/main.tf
+++ example/tgw-routes/main.tf
@@ -1,11 +1,22 @@
 module "tgw_routes" {
   source = "../../tgw-routes"
 
-  create_tgw_route_table         = var.create_tgw_route_table
-  create_tgw_routes              = var.create_tgw_routes
-  transit_gateway_id             = var.transit_gateway_id
-  transit_gateway_attachment_id  = var.transit_gateway_attachment_id
-  transit_gateway_route_table_id = var.transit_gateway_route_table_id
+  create_tgw_route_table         = true
+  create_tgw_routes              = true
+  transit_gateway_id             = "tgw-0123456789"
+  transit_gateway_attachment_id  = "tgw-attach-0123456789"
+  transit_gateway_route_table_id = "tgw-rtb-0123456789"
 
-  tgw_routes = var.tgw_routes
+  tgw_routes = [
+    {
+      destination_cidr_block         = "10.0.0.0/16"
+      transit_gateway_attachment_id  = "tgw-attach-0123456789"
+      blackhole                      = false
+    },
+    {
+      destination_cidr_block         = "192.168.0.0/16"
+      transit_gateway_attachment_id  = "tgw-attach-9876543210"
+      blackhole                      = false
+    }
+  ]
 }
